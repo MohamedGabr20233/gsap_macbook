3ce232cd0c4c69dc8ddac1de7a2be279d5d44612
Mohamed_Gabr <mohamedgbr20233@gmail.com>
2026-05-10 15:06:47 +0300

    feat: add initial project structure with React, TypeScript, and Tailwind CSS
    - Created main application files including App.tsx and main.tsx
    - Implemented NavBar component with navigation links
    - Added constants for navigation links
    - Included custom fonts and styles in index.css
    - Configured TypeScript with appropriate settings in tsconfig files
    - Set up Vite configuration for React and Tailwind CSS
    - Added video assets for features and game
    - Included Vite logo as SVG asset

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <div className="">
+        <NavBar/>
+        <main></main>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/constants/index.ts
@@ -0,0 +1,24 @@
+   export const navLinks = [
+        {
+            label:"store"
+        }
+        ,
+        {
+            label:"mac"
+        }
+        ,
+        {
+            label:"iPhone"
+        }
+        ,
+        {
+            label:"Watch"
+        }
+,
+    {                label:"Vision"
+           }
+            ,
+            {
+                label:"AirPods"
+            }
+    ];
