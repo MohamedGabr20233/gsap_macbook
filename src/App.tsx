71082cb76585a9103e296c4990b3b9724948c17e
MohamedGabr20233 <mohamedgbr20233@gmail.com>
2026-05-16 18:27:29 +0300

    feat: add Showcase component with GSAP animations and responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import ProductViewer from "./components/ProductViewer";
 import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
+import Showcase from "./components/Showcase";
 
 // we have to make plugin our gsap manually to make it globally accessibly
 gsap.registerPlugin(ScrollTrigger);
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 };
